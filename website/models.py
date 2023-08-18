336466ac471c98bc4e577fd7358a99b288bcc09f
Amitay Oren <amitay.oren@gmail.com>
2023-08-18 22:42:00 +0300

    Update models.py

--- website/models.py
+++ website/models.py
@@ -28,3 +28,5 @@ class Feedback(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(1000), nullable=False)
     bot_id = db.Column(db.String(36), db.ForeignKey('note.bot_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
